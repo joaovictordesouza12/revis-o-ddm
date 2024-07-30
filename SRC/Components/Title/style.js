cc0e8f5617ed6091365e8798ea99130addc1b7a1
joaovictordesouza12 <ramdom.pl4yer@gmail.com>
2024-07-30 12:33:08 -0300

    primeiro commit

---
+++ SRC/Components/Title/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native-web";
+
+const styles = StyleSheet.create({
+    BoxTitle: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+    texTitle: {
+        color:"#EA3829",
+        fontSize: 24,
+        fontweight: "bold",
+    
+    },
+});
+export default styles
