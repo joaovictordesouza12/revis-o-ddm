66e7733048c6c2dfa01ada2375fcd4c9f1632062
joaovictordesouza12 <ramdom.pl4yer@gmail.com>
2024-07-30 12:30:53 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import{StyleSheet, Text, View} from 'react-native';
+import Title from './SRC/Components/Title/index';
+
+
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text>Ana & Jão</Text>
+      <Title/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#e0e5e5',
+    paddingTop:80,
+  },
+});
